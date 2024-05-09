1a30d3b04e4b82159ac56927c6bdc238d13a8bef
j2002chen <j.2002chen@gmail.com>
2024-05-08 23:32:34 -0400

    added components

---
+++ components/header.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <Link href="/" className={styles.navLink}>About</Link>
+        <Link href="/experience" className={styles.navLink}>Experience</Link>
+      </nav>
+    </header>
+  );
+}

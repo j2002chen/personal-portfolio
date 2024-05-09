4f057e5e7fb37fcbec5b383a2891866d5bb20ebf
j2002chen <j.2002chen@gmail.com>
2024-05-08 23:32:41 -0400

    added pages

---
+++ pages/experience.tsx
@@ -0,0 +1,15 @@
+import Header from '../components/header';
+import Footer from '../components/footer';
+
+export default function Experience() {
+  return (
+    <>
+      <Header />
+      <div>
+        {/* Add your content here */}
+        <p>Temp</p>
+      </div>
+      <Footer />
+    </>
+  );
+}

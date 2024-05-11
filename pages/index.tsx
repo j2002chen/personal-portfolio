48bb7e25c46288bf4980b9f3670c942b8b742548
j2002chen <j.2002chen@gmail.com>
2024-05-10 22:52:23 -0400

    homke page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import Head from 'next/head';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -14,11 +14,36 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main>
-        <p>
-          Hey! I&apos;m James Chen, a 3rd-year Mechatronics Engineering Student
-          at the University of Waterloo.
-        </p>
+      <main className={styles.main}>
+      <div className={styles.intro}>
+  <div className={styles.portrait}>
+    <Image
+      src="/temp-placeholder.jpg"
+      alt="James Chen"
+      width={500}
+      height={500}
+      className={styles.image}
+    />
+  </div>
+  <h1 className={styles['intro-text']}>Hey! I&apos;m James Chen</h1>
+</div>
+        <div className={styles.body}>
+          <h2>ABOUT ME</h2>
+          <p>
+            I&apos;m a 3rd-year Mechatronics Engineering Student
+            at the University of Waterloo. I have completed internships working in full-stack development, machine learning, and data engineering.
+          </p>
+          <p>
+            Some of my hobbies include:
+          </p>
+          <ul>
+            <li>Playing guitar</li>
+            <li>Cooking</li>
+            <li>Watching movies</li>
+            <li>Playing video games</li>
+            <li>Reading</li>
+          </ul>
+        </div>
       </main>
       <Footer />
     </>

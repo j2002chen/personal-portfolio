1c633966f3a1bc4256a80d3d3ad9c1422c89e164
jchen1707 <j.2002chen@gmail.com>
2024-10-06 00:13:02 -0400

    redid design for primary pages

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -1,78 +1,16 @@
 import Header from '../components/header';
 import Footer from '../components/footer';
 import styles from '../styles/Experience.module.css';
+import WorkExperience from '../components/job-history';
 
 export default function Experience() {
   return (
     <div className={styles.container}>
-      <Header />
+      <Header className={styles.header} />
       <main className={styles.main}>
-        <h2 className={styles.companyName}>
-          Mircom Technologies Ltd. - Software Development Engineer Intern
-        </h2>
-        <p className={styles.description}>
-          Fire and Security and Security Solutions.
-        </p>
-        <h2 className={styles.companyName}>
-          Swan Cloud Inc. - Software Engineer Intern
-        </h2>
-        <p className={styles.description}>
-          Global cloud compute and storage platform leveraging their L2
-          Ethereum Solution for payment channels
-        </p>
-        <p className={styles.jobDetails}>
-          Worked as a fullstack developer to ship several critical features
-          handling high traffic cryptocurrency transaction channels and
-          optimized various endpoints that facilitated their cloud compute
-          platform for performance. Designed incident response and alerting
-          systems to notify staff of anomolies, service disruptions, security
-          issues, and failures.
-        </p>
-        <h2 className={styles.companyName}>
-          Healthcare Systems R&A - Software Engineer Intern
-        </h2>
-        <p className={styles.description}>
-          Canadian medical data analysis and research institution creating a
-          predictive modelling platform with leveraging various machine
-          learning methods
-        </p>
-        <p className={styles.jobDetails}>
-          Led graph neural network model optimization processes for classification
-          task predicting patients&apos; likeliness of developing
-          cardiovascular disorders as well as various regression models
-          predicting the progression of users&apos; self reported biometrics.
-          Enhanced the scalability and security of both DevOps and MLOps
-          infrastructure, streamlining and automating deployments as well as
-          optimizing backend code for performance.
-        </p>
-        <h2 className={styles.companyName}>
-          Purolator - Data Engineer Intern
-        </h2>
-        <p className={styles.description}>
-          Internal reporting and data analytics platform for Canadian courier
-          service for mailing, shipping and package tracking
-        </p>
-        <p className={styles.jobDetails}>
-          Worked on a large scale data migration project designing
-          orchestration workflows and ETL processes supporting
-          Purolator&apos;s data analytics platform licensing agreement changes
-          to save time and costs.
-        </p>
-        <h2 className={styles.companyName}>
-          Employment & Social Development Canada(ESDC) - Integrated Service
-          Coordination Centre - Data Engineer Intern
-        </h2>
-        <p className={styles.description}>
-          Centralized data analytics and visualization hub that creates
-          internal reporting systems for ESDC services such as web-forms, call
-          centres and employee key performance metrics
-        </p>
-        <p className={styles.jobDetails}>
-          Developed and optimized data warehouse ETL processes, optimized
-          database architecture and designed interactive dashboards.
-        </p>
+        <WorkExperience />
       </main>
-      <Footer />
+      <Footer className={styles.footer} />
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,11 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 import Image from 'next/image';
 import IconCloud from '../components/icon-cloud';
+import Education from '../components/education';
 
 const slugs = [
   "typescript",
-  "javascript",
   "cplusplus",
-  "MQTT",
-  "QTT",
   "react",
   "python",
   "go",
@@ -27,60 +25,60 @@ const slugs = [
   "jenkins",  
   "kubernetes",
   "git",
-  "jira",
 ];
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>James Chen Portfolio</title>
-        <meta name="description" content="James Chen Portfolio" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <main className={styles.main}>
-        <div className={styles.intro}>
-          <div className={styles.portrait}>
-            <Image
-              src="./portrait.jpg"
-              alt="James Chen"
-              width={500}
-              height={500}
-              className={styles.image}
-            />
+    <div className={styles.pageContainer}>
+      <div className={styles.container} style={{ backgroundColor: 'var(--background-color)', color: 'var(--text-color)' }}>
+        <Head>
+          <title>James Chen Portfolio</title>
+          <meta name="description" content="James Chen Portfolio" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header />
+        <main className={styles.main}>
+          <div className={styles.intro}>
+            <div className={styles.portrait}>
+              <Image
+                src="./portrait.jpg"
+                alt="James Chen"
+                width={500}
+                height={500}
+                className={styles.image}
+              />
+            </div>
+            <h1 className={styles['intro-text']}>
+              {"Hey! I'm James Chen".split("").map((char, index) => (
+                <span key={index} style={{ animationDelay: `${index * 0.1}s` }}>
+                  {char === " " ? "\u00A0" : char}
+                </span>
+              ))}
+            </h1>
           </div>
-          <h1 className={styles['intro-text']}>
-            {"Hey! I'm James Chen".split("").map((char, index) => (
-              <span key={index} style={{ animationDelay: `${index * 0.1}s` }}>
-                {char === " " ? "\u00A0" : char}
-              </span>
-            ))}
-          </h1>
-        </div>
-        <div className={styles.body}>
-          <h2>ABOUT ME</h2>
-          <p>
-            I&apos;m a 3rd-year Mechatronics Engineering Student at the
-            University of Waterloo. I have completed internships working in
-            full-stack development and data engineering. 
-            I am currently seeking Summer 2025 SWE internships!
-          </p>
-          <p>Some of my hobbies include:</p>
-          <ul>
-            <li>Playing guitar</li>
-            <li>Cooking</li>
-            <li>Watching movies</li>
-            <li>Playing video games</li>
-            <li>Reading</li>
-          </ul>
-        </div>
-        <div className="relative flex size-full max-w-lg items-center justify-center overflow-hidden rounded-lg border bg-background px-20 pb-20 pt-8 ">
-      <IconCloud iconSlugs={slugs} />
-    </div>
-      </main>
-      <Footer />
+          <div className={styles.body}>
+            <h2>ABOUT ME</h2>
+            <p className={styles['narrow-text']}>
+              I&apos;m a 3rd-year Mechatronics Engineering Student at the
+              University of Waterloo. I have completed internships working on
+              full-stack development and data engineering. 
+              I am currently seeking Summer 2025 SWE internships!
+            </p>
+          </div>
+          <div className={`${styles.iconCloudContainer} relative flex size-full max-w-lg items-center justify-center overflow-hidden rounded-lg border bg-background px-20 pb-20 pt-8`}>
+            <IconCloud iconSlugs={slugs} />
+          </div>
+          <Education
+            institution="University of Waterloo"
+            program="Bachelor of Applied Science in Mechatronics Engineering"
+            period="2021 - 2026"
+            logo="./uwaterloo.png"
+            website="https://uwaterloo.ca/"
+          />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }

17d4830d57128f4c6606d9820534e739340cc741
j2002chen <j.2002chen@gmail.com>
2025-01-12 22:26:48 -0500

    fixed build issue on cloud element

--- components/icon-cloud.tsx
+++ components/icon-cloud.tsx
@@ -9,28 +9,16 @@ export type DynamicCloudProps = {
 
 type IconData = Awaited<ReturnType<typeof fetchSimpleIcons>>;
 
-/*************  ✨ Codeium Command ⭐  *************/
-/**
- * DynamicCloud component fetches and displays a cloud of icons based on provided slugs.
- * It uses the `react-icon-cloud` library to render icons and relies on the current theme color.
- * 
- * Props:
- * - iconSlugs: An array of strings representing the slugs of icons to be fetched and displayed.
- * 
- * The component fetches icon data asynchronously and updates its state. It then renders the icons
- * using a custom renderer, applying the current theme's color.
- */
-
-/******  10bb2199-3563-46a6-8b64-bcc328eb8692  *******/
 export const DynamicCloud = (props: DynamicCloudProps) => {
   const { color } = useTheme();
   const [data, setData] = React.useState<IconData>();
   React.useEffect(() => {
     fetchSimpleIcons({ slugs: props.iconSlugs }).then(setData);
   }, [props.iconSlugs]);
+
   const renderedIcons = React.useMemo(() => {
     if (!data) {
-      return null;
+      return [];
     }
 
     const icons = [];
